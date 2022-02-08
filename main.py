20c68632f0f460136b18375ccbfc631de7fff135
Quinn Henry <78760148+Brit-Boi@users.noreply.github.com>
2022-02-07 21:29:12 -0500

    Fix main.py to accommodate for Args.py and Ruckus.py

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
-# guestPassCreation.py
-# Generates a random 3-word phrase, and modifies specified WLANS SSIDs for each zone
-# API Documentation can be found on the controller GUI, under "Administration" > "REST API"
-
-# To be run on the following dates:
-#   Oct 30
-#       - End of MP1
-#   Dec 30
-#       - End of MP2
-#   Feb 28
-#       - End of MP3
-#   June 15
-#       - End of MP4
-#   July 30
-#       - Mid-End of Summer
-
-# DEPENDENCIES NEEDED TO INSTALL FOR SCRIPT TO WORK
-# python3 -m pip install requests
-# python3 -m pip install json
+# main.py
+# Generates a random 2-word phrase, and modifies specified WLANS SSIDs for each zone
 
 # Import OS for directory usage
 import os
-# Import requests for API interfacing
-import requests
 # Import argument parser
 import argparse
 # Import local files
-from src import Ruckus, ModifyGuestPassphrase, SendEmail, WordGen
+from src import Ruckus, SendEmail, WordGen, Args
 
 # Set location of current directory for reliable file opening
 __location__ = os.path.realpath(
@@ -36,33 +17,18 @@ __location__ = os.path.realpath(
         )
 )
 
-# Initialize parser
-parser = argparse.ArgumentParser(description="Generates a random 3-word phrase, and modifies specified WLANS SSIDs for each zone")
+print("\n\n")
 
-# Help Prompts
-userPrompt = "Set username to log in with"
-passPrompt = "Set password to log in with"
-controllerPrompt = "Set DNS name to log in to, best if use FQDN"
-
-# Add arguments
-parser.add_argument("-u", "--Username", help = userPrompt, required=True)
-parser.add_argument("-p", "--Password", help = passPrompt, required=True)
-parser.add_argument("-c", "--Controller", help = controllerPrompt, required=True)
-
-# Read arguments from command line
-args = parser.parse_args()
-
-# Set given arguments to variables
-username = args.Username
-password = args.Password
-controller = args.Controller
+args = Args.getArgs()
 
 
-logon = Ruckus.login(username, password, controller)
+serviceTicket = Ruckus.login(args['username'], args['password'], args['controller'])
 
-serviceTicket = Ruckus.getServiceTicket(logon)
+zones = Ruckus.getZones(serviceTicket, args['controller'])
+print(zones + "\n\n")
 
-print(Ruckus.getZones(serviceTicket, controller))
+wlans = Ruckus.getWlans()
 
 # Generate random 2-word phrase
 passphrase = WordGen.createKey(__location__)
+print(passphrase + "\n\n")
