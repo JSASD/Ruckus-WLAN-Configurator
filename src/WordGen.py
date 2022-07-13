ffb2f894eae0afbdf11829a3154109d4f865509d
Quinn Henry <78760148+TheQuib@users.noreply.github.com>
2022-07-13 16:17:24 -0400

    ✍️ Logging everything

--- src/WordGen.py
+++ src/WordGen.py
@@ -1,5 +1,6 @@
 import random
 import os
+import logging
 
 # Generates random 3-word phrases, "-" delimited
 def createKey(__location__):
@@ -20,20 +21,26 @@ def createKey(__location__):
     file = open(os.path.join(__location__,'wordList.txt'), 'r')
     # Read file line by line
     wordList = file.readlines()
+
     # Initialize list of words
     words = []
     # Add each word from wordlist into the 'words' list
     for line in wordList:
         words.append("{}".format(line.strip()))
+
     # Close the file
     file.close()
+
     # Number of words to be placed in phrase
     phraseSize = 2
     # Initialize list to place the phrase in
     phrase = []
     for i in range(phraseSize):
         phrase.append(words[random.randrange(1,len(words))])
+
     # Create key and store in variable
     key = '-'.join(str(x) for x in phrase)
-    # Print the key
+
+    logging.info("Key generated")
+    logging.debug(key)
     return(key)
